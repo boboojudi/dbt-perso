9a16e7d86b81c160217b1d3f6445ef0860725c4f
Julien Diennet <judiennet@gmail.com>
2025-06-26 11:50:14 +0000

    macros and packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    round(amount / 100.0, 2) as payment_amount,
+    {{ cents_to_dollars('amount', 4) }} as payment_amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
